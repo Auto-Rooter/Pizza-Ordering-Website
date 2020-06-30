13c0d4311ff10147e859187e40ee1172a4dac637
Autorooter <hadi.assalem@gmail.com>
2020-06-30 06:51:51 +0200

    Fix Mixed content

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,6 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
