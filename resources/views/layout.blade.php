3204559a3544b21030d9e2d945120634902b7a42
Autorooter <hadi.assalem@gmail.com>
2020-07-01 05:00:49 +0200

    force https requests

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,8 @@
 <head>
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-    <title>@yield('title')</title>
+    <link rel="icon" href="{{ URL::to('ssss.png') }}">
+    <title>Pizza :)</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
